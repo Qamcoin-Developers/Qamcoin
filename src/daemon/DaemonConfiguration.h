aabc7d79988e196799aa669e986f0d281fbb8036
ijmcglynn <49611176+ijmcglynn@users.noreply.github.com>
2020-09-02 16:38:39 +0100

    Update DaemonConfiguration.h

--- src/daemon/DaemonConfiguration.h
+++ src/daemon/DaemonConfiguration.h
@@ -1,5 +1,5 @@
 // Copyright (c) 2018-2019, The TurtleCoin Developers
-// Copyright (c) 2019, The CyprusCoin Developers
+// Copyright (c) 20, The QamCoin Developers
 //
 // Please see the included LICENSE file for more information.
 
